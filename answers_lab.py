94f0baab7208bb40b3617e2437872f10ff9d6c7c
Kevin Ziegler <36745753+Kevin-Ziegler@users.noreply.github.com>
2023-10-03 12:29:16 -0400

    Create answers_lab.py
    Added answers_lab.py

---
+++ answers_lab.py
@@ -0,0 +1,7 @@
+def find_min_mc(n, xl, xr, f):
+    # Your code here
+    return (xmin, ymin), minval
+
+def find_min_mc_wzoom(n, xl, xr, f, nzoom):
+    # Your code here
+    return (xmin, ymin), minval
